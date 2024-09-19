bedb21c6c86423b57145d347ee07ba9fbeaf04a9
Fred Dezorzi <fred.dezorzi@gmail.com>
2024-09-19 15:52:19 -0300

    Mudanca nome endpoint  permanentPassword

--- src/permanentPassword/index.js
+++ src/permanentPassword/index.js
@@ -3,22 +3,22 @@ import { generateCognitoSecretHash } from  '../utils/index.js';
 
 const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
 
-export const challengeHandler = async (event) => {
+export const permanentPasswordHandler = async (event) => {
   try {
     const credentials = JSON.parse(event.body);
     const params = {
       ChallengeName: "NEW_PASSWORD_REQUIRED",
       ClientId: process.env.COGNITO_ID,
       ChallengeResponses: {
-        USERNAME: credentials.USER_ID_FOR_SRP,
+        USERNAME: credentials.userIdForSrp,
         NEW_PASSWORD: credentials.newPassword,
         SECRET_HASH: generateCognitoSecretHash(
           process.env.COGNITO_ID ,
           process.env.COGNITO_SECRET,
-          credentials.USER_ID_FOR_SRP,
+          credentials.userIdForSrp,
         ),
       },
-      Session: credentials.Session,
+      Session: credentials.session,
     }
     const command = new RespondToAuthChallengeCommand(params);
     const responseBody = await client.send(command);
